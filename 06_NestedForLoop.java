66aebda24d768ad296a01215a30169b47f0d13b6
Praduman Jaiswal <pradumanjaiswal625@gmail.com>
2022-12-25 19:54:35 +0530

    loop

---
+++ 06_NestedForLoop.java
@@ -0,0 +1,16 @@
+package java_programs;
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<5; i++) {
+			for(int j = 0; j<5; j++) {
+				System.out.println(i+" -- "+j);
+			}
+		}
+		
+
+	}
+
+}
