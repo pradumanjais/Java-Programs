357187b65c17bdecab11242bccfaa2f53a57d8f7
Praduman Jaiswal <pradumanjaiswal625@gmail.com>
2022-12-25 19:54:52 +0530

    loop

---
+++ 07_WhileLoop.java
@@ -0,0 +1,15 @@
+package java_programs;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		while(num<=10) {
+			System.out.println("Num value is: "+num);
+			num++;
+		}
+
+	}
+
+}
