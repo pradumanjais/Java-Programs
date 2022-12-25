a7031f5387b745f1001e55d529460b45220c448b
Praduman Jaiswal <pradumanjaiswal625@gmail.com>
2022-12-25 19:53:26 +0530

    if-else

---
+++ 03_IfCondition.java
@@ -0,0 +1,18 @@
+package java_programs;
+
+public class IfCondition {
+
+	public static void main(String[] args) {
+		
+		int a = 20, b = 30;
+		if(a==b && a<b) {
+			System.out.println("Condition True");
+		}
+		else {
+			System.out.println("Condition False");
+		}
+
+	}
+
+}
+ 
