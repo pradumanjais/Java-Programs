b2c09783ce1802d8532ac34a7419b84716ade8a0
Praduman Jaiswal <pradumanjaiswal625@gmail.com>
2022-12-25 19:53:04 +0530

    datatype

---
+++ 02_DatatypesInJava.java
@@ -0,0 +1,26 @@
+package java_programs;
+
+public class DatatypesInJava {
+
+	public static void main(String[] args) {
+		
+		byte a = 0; // 1 byte	  by default 0
+		short b = 0; // 2 byte	  by default 0
+		int c = 20; // 4 byte     by default 0
+		float f = 4.2f; // 4 byte by default 0.0f
+		double d = 12; // 8 byte  by default 0.0d
+		char ch = 'A'; // 2 byte  by default '\u0000'
+		boolean value = true; // 1 bit   by default false
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(ch);
+		System.out.println(value);
+		
+		// NOTE: println is used to print in new line . print is used to print in same line.
+	}
+
+}
