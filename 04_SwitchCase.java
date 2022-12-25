be167ae494bfa5207a556707a797e7a05fd4d0bd
Praduman Jaiswal <pradumanjaiswal625@gmail.com>
2022-12-25 19:53:51 +0530

    switch-case

---
+++ 04_SwitchCase.java
@@ -0,0 +1,35 @@
+package java_programs;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 6;
+		int c = a-b;
+		
+		switch(c) {
+		case 1:
+			System.out.println("C value is 1");
+			break;
+			
+		case 2:
+			System.out.println("C value is 2");
+			break;
+		
+		case 3:
+			System.out.println("C value is 3");
+			break;
+			
+		case 4:
+			System.out.println("C value is 4");
+			break;
+			
+		default:
+			System.out.println("C value is out of Range");
+			break;
+			 
+		}
+		
+	}
+
+}
