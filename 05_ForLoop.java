38cac7f60aedfb6bb26ce26a11857bc862f89760
Praduman Jaiswal <pradumanjaiswal625@gmail.com>
2022-12-25 19:54:17 +0530

    loop

---
+++ 05_ForLoop.java
@@ -0,0 +1,16 @@
+package java_programs;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<5; i++) {
+			int a = 10, b = 20;
+			int c = a*b;
+			System.out.println(c+" - "+i);
+		}
+		
+
+	}
+
+}
